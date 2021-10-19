972b5ac1f4cff931d9bb4bb6d663d148ba2e9fbf
Felipe Martins <felipemartins96@gmail.com>
2021-10-19 17:53:09 -0300

    code formatting

--- src/robosim/robosim_py.cpp
+++ src/robosim/robosim_py.cpp
@@ -11,16 +11,27 @@ using vvd = std::vector<std::vector<double>>;
 
 struct VSS {
     VSS(int fieldType, int nRobotsBlue, int nRobotsYellow, int timeStep_ms,
-        const vd& ballPos, const vvd& blueRobotsPos, const vvd& yellowRobotsPos) : m_fieldType(fieldType), m_nRobotsBlue(nRobotsBlue), m_nRobotsYellow(nRobotsYellow), m_timeStep_ms(timeStep_ms){
+        const vd &ballPos, const vvd &blueRobotsPos, const vvd &yellowRobotsPos) : m_fieldType(fieldType),
+                                                                                   m_nRobotsBlue(nRobotsBlue),
+                                                                                   m_nRobotsYellow(nRobotsYellow),
+                                                                                   m_timeStep_ms(timeStep_ms) {
         m_world = new VSSWorld(m_fieldType, m_nRobotsBlue, m_nRobotsYellow, m_timeStep_ms / 1000.0,
-                             ballPos, blueRobotsPos, yellowRobotsPos);
+                               ballPos, blueRobotsPos, yellowRobotsPos);
     }
-    ~VSS() { delete m_world;}
+
+    ~VSS() { delete m_world; }
+
     void step(vvd actions) const { m_world->step(std::move(actions)); }
+
     vd getState() const { return m_world->getState(); }
-    void reset(const vd& ballPos, const vvd& blueRobotsPos, const vvd& yellowRobotsPos) {delete m_world;  m_world = new VSSWorld(m_fieldType, m_nRobotsBlue, m_nRobotsYellow, m_timeStep_ms / 1000.0,
-                                                                                                                                 ballPos, blueRobotsPos, yellowRobotsPos);}
-    std::unordered_map<std::string, double> getFieldParams() const {return m_world->getFieldParams();}
+
+    void reset(const vd &ballPos, const vvd &blueRobotsPos, const vvd &yellowRobotsPos) {
+        delete m_world;
+        m_world = new VSSWorld(m_fieldType, m_nRobotsBlue, m_nRobotsYellow, m_timeStep_ms / 1000.0,
+                               ballPos, blueRobotsPos, yellowRobotsPos);
+    }
+
+    std::unordered_map<std::string, double> getFieldParams() const { return m_world->getFieldParams(); }
 
     VSSWorld *m_world;
     int m_fieldType, m_nRobotsBlue, m_nRobotsYellow, m_timeStep_ms;
@@ -28,16 +39,27 @@ struct VSS {
 
 struct SSL {
     SSL(int fieldType, int nRobotsBlue, int nRobotsYellow, int timeStep_ms,
-        const vd& ballPos, const vvd& blueRobotsPos, const vvd& yellowRobotsPos) : m_fieldType(fieldType), m_nRobotsBlue(nRobotsBlue), m_nRobotsYellow(nRobotsYellow), m_timeStep_ms(timeStep_ms){
+        const vd &ballPos, const vvd &blueRobotsPos, const vvd &yellowRobotsPos) : m_fieldType(fieldType),
+                                                                                   m_nRobotsBlue(nRobotsBlue),
+                                                                                   m_nRobotsYellow(nRobotsYellow),
+                                                                                   m_timeStep_ms(timeStep_ms) {
         m_world = new SSLWorld(m_fieldType, m_nRobotsBlue, m_nRobotsYellow, m_timeStep_ms / 1000.0,
                                ballPos, blueRobotsPos, yellowRobotsPos);
     }
-    ~SSL() { delete m_world;}
+
+    ~SSL() { delete m_world; }
+
     void step(vvd actions) const { m_world->step(std::move(actions)); }
+
     vd getState() const { return m_world->getState(); }
-    void reset(const vd& ballPos, const vvd& blueRobotsPos, const vvd& yellowRobotsPos) {delete m_world;  m_world = new SSLWorld(m_fieldType, m_nRobotsBlue, m_nRobotsYellow, m_timeStep_ms / 1000.0,
-                                                                                                                                 ballPos, blueRobotsPos, yellowRobotsPos);}
-    std::unordered_map<std::string, double> getFieldParams() const {return m_world->getFieldParams();}
+
+    void reset(const vd &ballPos, const vvd &blueRobotsPos, const vvd &yellowRobotsPos) {
+        delete m_world;
+        m_world = new SSLWorld(m_fieldType, m_nRobotsBlue, m_nRobotsYellow, m_timeStep_ms / 1000.0,
+                               ballPos, blueRobotsPos, yellowRobotsPos);
+    }
+
+    std::unordered_map<std::string, double> getFieldParams() const { return m_world->getFieldParams(); }
 
     SSLWorld *m_world;
     int m_fieldType, m_nRobotsBlue, m_nRobotsYellow, m_timeStep_ms;
@@ -46,11 +68,11 @@ struct SSL {
 
 PYBIND11_MODULE(robosim, m) {
     py::class_<VSS>(m, "VSS")
-        .def(py::init<int, int, int, int, vd, vvd, vvd>())
-        .def("step", &VSS::step)
-        .def("get_state", &VSS::getState)
-        .def("reset", &VSS::reset)
-        .def("get_field_params", &VSS::getFieldParams);
+            .def(py::init<int, int, int, int, vd, vvd, vvd>())
+            .def("step", &VSS::step)
+            .def("get_state", &VSS::getState)
+            .def("reset", &VSS::reset)
+            .def("get_field_params", &VSS::getFieldParams);
 
     py::class_<SSL>(m, "SSL")
             .def(py::init<int, int, int, int, vd, vvd, vvd>())
