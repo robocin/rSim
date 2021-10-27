b413932b7b4bbc0fc399f6a979e4cfcc4f56264f
Felipe Martins <felipemartins96@gmail.com>
2021-10-27 16:12:50 -0300

    fix(GithubActions): fix checkout command for scm version
    test(Actions): entrypoint tests
    
    
    hotfix(Actions): wrong command
    
    
    hotfix(CI): fix release to PyPI action

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -2,11 +2,12 @@
 
 echo "Starting entrypoint.sh"
 
-Build wheels for different python versions
+echo "Build wheels for different python versions"
 for PYBIN in /opt/python/*/bin; do #3.7,3.8,3.9
     echo "builind dist for ${PYBIN}"
+    "${PYBIN}/pip" install pip --upgrade #precisa para buildar
     "${PYBIN}/pip" install scikit-build #precisa para buildar
-    "${PYBIN}/python" setup.py bdist_wheel
+    "${PYBIN}/python" -m build --wheel --outdir dist/ .
 done
 
 echo "auditwheels"
@@ -18,4 +19,4 @@ rm -rf dist/
 mv -v wheelhouse/ dist/
 
 echo "setup sdist"
-/opt/python/pp37-pypy37_pp73/bin/python setup.py sdist
+/opt/python/pp37-pypy37_pp73/bin/python -m build --sdist --outdir dist/
