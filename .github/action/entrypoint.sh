b89cd3911ae064d08bfb4403ef52bdb192fcf609
Felipe Martins <felipemartins96@gmail.com>
2021-10-27 12:13:23 -0300

    CI action to publish package to pypi and test pypi with manual dispatch

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -2,15 +2,12 @@
 
 echo "Starting entrypoint.sh"
 
-# Build wheels for different python versions
-# for PYBIN in /opt/python/*/bin; do #3.7,3.8,3.9
-#     echo "builind dist for ${PYBIN}"
-#     "${PYBIN}/pip" install scikit-build #precisa para buildar
-#     "${PYBIN}/python" setup.py bdist_wheel
-# done
-
-/opt/python/pp37-pypy37_pp73/bin/pip install scikit-build
-/opt/python/pp37-pypy37_pp73/bin/python setup.py bdist_wheel
+Build wheels for different python versions
+for PYBIN in /opt/python/*/bin; do #3.7,3.8,3.9
+    echo "builind dist for ${PYBIN}"
+    "${PYBIN}/pip" install scikit-build #precisa para buildar
+    "${PYBIN}/python" setup.py bdist_wheel
+done
 
 echo "auditwheels"
 for WHEEL in dist/*; do #3.7,3.8,3.9
@@ -20,5 +17,5 @@ done
 rm -rf dist/
 mv -v wheelhouse/ dist/
 
-echo "sdist"
+echo "setup sdist"
 /opt/python/pp37-pypy37_pp73/bin/python setup.py sdist
